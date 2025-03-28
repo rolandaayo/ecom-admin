103ebc8a9fdc35971d3cf24923e7192bc5c400fd
rolandaayo <ibiwoyeroland@gmail.com>
2025-03-28 09:41:58 +0100

    axios fixed

--- lib/axios-config.ts
+++ lib/axios-config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001',
+  baseURL: 'https://ecom-admin-backend.vercel.app/',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
